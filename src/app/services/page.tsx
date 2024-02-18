f98a7f9600fe31ff35f7721e9bf4b26bd4a1b2f6
shayanabedi98 <shayanabedi@hotmail.com>
2024-02-17 19:29:19 -0500

    fix bug where 2 new services components wont show

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,11 +1,15 @@
 import OurServices from "@/components/services/OurServices";
 import Heading from "../../components/services/Heading";
+import StartJourney from "@/components/services/StartJourney";
+import FAQ from "@/components/services/FAQ";
 
 export default function Services() {
     return (
         <main>
             <Heading />
             <OurServices />
+            <StartJourney />
+            <FAQ />
         </main>
     )
 }
