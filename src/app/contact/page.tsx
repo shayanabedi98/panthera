7431c18b262b1908fd69185adda91c9f6d1933f6
shayanabedi98 <shayanabedi@hotmail.com>
2024-02-20 14:16:29 -0500

    Add contact form to contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactForm from "@/components/contact/ContactForm";
+
+
+export default function Contact() {
+    
+  return (
+    <div>
+      <h1>Contact</h1>
+        <ContactForm />
+    </div>
+  );
+}
