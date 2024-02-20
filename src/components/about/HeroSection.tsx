00c44805358f4ec258a889f13f3b9b8ff52c3f32
delabahmani <delabahmani@gmail.com>
2024-02-19 21:02:46 -0500

    &apos; fix on HeroSection

--- src/components/about/HeroSection.tsx
+++ src/components/about/HeroSection.tsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
       <div className="flex flex-col items-start">
         <h1 className="font-semibold text-7xl mb-10">a bit about us</h1>
         <p className="font-secondary text-white w-[600px] leading-7">
-        Located in Toronto, our digital marketing startup is built on the foundation of collaboration, innovation, and expertise. Our team brings together a diverse range of skills and experiences, with a common goal: to empower businesses to thrive in the digital world. From web development and design to crafting effective marketing strategies, we leverage our combined knowledge to deliver impactful solutions tailored to each client's unique needs. We believe in the power of creativity, data-driven insights, and continuous learning to drive success for our clients. Let us be your partner in achieving your digital marketing goals.
+        Located in Toronto, our digital marketing startup is built on the foundation of collaboration, innovation, and expertise. Our team brings together a diverse range of skills and experiences, with a common goal: to empower businesses to thrive in the digital world. From web development and design to crafting effective marketing strategies, we leverage our combined knowledge to deliver impactful solutions tailored to each client&apos;s unique needs. We believe in the power of creativity, data-driven insights, and continuous learning to drive success for our clients. Let us be your partner in achieving your digital marketing goals.
         </p>
       </div>
       <div className="flex justify-end">
