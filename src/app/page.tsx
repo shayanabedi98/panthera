293b58ecce7976192285a75164a92fed747942b8
delabahmani <delabahmani@gmail.com>
2024-02-27 22:30:18 -0500

    Added metadata to layout and all pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,23 @@ import OurServices from "@/components/home/OurServices";
 import Technologies from "@/components/home/Technologies";
 import WhatIsPanthera from "@/components/home/WhatIsPanthera";
 
+export const metadata = {
+  title: "Pantheras | Home",
+  description: "",
+  keywords: [
+    "integrated digital marketing solutions",
+    "custom web design and development",
+    "ux/ui design solutions",
+    "full stack javascript development",
+    "next.js 14 dynamic web solutions",
+    "content marketing strategy",
+    "branding and graphic design solutions",
+    "seo services",
+    "social Media strategy and engagement",
+    "social media content and email marketing",
+  ],
+};
+
 export default function Home() {
   return (
     <main>
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,13 +3,30 @@ import Heading from "../../components/services/Heading";
 import StartJourney from "@/components/services/StartJourney";
 import FAQ from "@/components/services/FAQ";
 
+export const metadata = {
+  title: "Pantheras | Services",
+  description: "",
+  keywords: [
+    "integrated digital marketing solutions",
+    "custom web design and development",
+    "ux/ui design solutions",
+    "full stack javascript development",
+    "next.js 14 dynamic web solutions",
+    "content marketing strategy",
+    "branding and graphic design solutions",
+    "seo services",
+    "social Media strategy and engagement",
+    "social media content and email marketing",
+  ],
+};
+
 export default function Services() {
-    return (
-        <main>
-            <Heading />
-            <OurServices />
-            <StartJourney />
-            <FAQ />
-        </main>
-    )
+  return (
+    <main>
+      <Heading />
+      <OurServices />
+      <StartJourney />
+      <FAQ />
+    </main>
+  );
 }
